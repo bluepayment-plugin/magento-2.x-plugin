e08815eb548f8c37d69648718f87bc7466cd93e7
Krzysztof Graman <zalazdi@gmail.com>
2019-05-03 11:30:38 +0200

    Magento 2.1 fix

--- Block/Form.php
+++ Block/Form.php
@@ -29,27 +29,19 @@ class Form extends \Magento\Payment\Block\Form
     protected $collectionFactory;
 
     /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
-     */
-    protected $scopeConfig;
-
-    /**
      * Form constructor.
      *
      * @param \Magento\Framework\View\Element\Template\Context                      $context
      * @param \BlueMedia\BluePayment\Model\ResourceModel\Gateways\CollectionFactory $collectionFactory
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface                    $scopeConfig
      * @param array                                                                 $data
      */
     public function __construct(
         Context           $context,
         CollectionFactory $collectionFactory,
-        ScopeConfigInterface $scopeConfig,
         array             $data = []
     ) {
         parent::__construct($context, $data);
         $this->collectionFactory = $collectionFactory;
-        $this->scopeConfig  = $scopeConfig;
     }
 
     /**
@@ -78,23 +70,5 @@ class Form extends \Magento\Payment\Block\Form
         $logo_src = $this->getViewFileUrl('BlueMedia_BluePayment::images/logo.jpg');
 
         return $logo_src != '' ? $logo_src : false;
-    }
-
-    /**
-     * @return \Magento\Framework\Phrase
-     */
-    public function getMethodTitle()
-    {
-        return __('Quick online payment');
-    }
-
-    public function getTitleAutomatic()
-    {
-        return $this->scopeConfig->getValue("payment/bluepayment/title_automatic");
-    }
-
-    public function getTitleBlik()
-    {
-        return $this->scopeConfig->getValue("payment/bluepayment/title_blik");
     }
 }
