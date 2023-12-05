0736ea1fa5a959fbcdc0e6d75520346e8b4dfef8
Krzysztof Graman <krzysztof@devmouse.pl>
2023-12-05 13:01:38 +0100

    MAGENTO-180 Update CSS for BLIK and texts

--- view/frontend/web/js/view/payment/method-renderer/bluepayment-separated.js
+++ view/frontend/web/js/view/payment/method-renderer/bluepayment-separated.js
@@ -98,12 +98,12 @@ define([
 
             if (gatewayId === model.gatewaysIds.alior_installments) {
                 return $t('0% installments and even 48 installments. %1')
-                    .replace('%1', '<a href="https://kalkulator.raty.aliorbank.pl/init?supervisor=B776&promotionList=B" target="_blank">' + $t('Check out other installment options') + '</a>');
+                    .replace('%1', '<a href="https://kalkulator.raty.aliorbank.pl/init?supervisor=B776&promotionList=B" target="_blank">' + $t('Learn more') + '</a>');
             }
 
             if (gatewayId === model.gatewaysIds.paypo) {
                 return $t('Pick up your purchases, check them out and pay later &mdash; in 30 days or in convenient installments. %1')
-                    .replace('%1', '<a href="https://start.paypo.pl/" target="_blank">' + $t('Find out the details') + '</a>');
+                    .replace('%1', '<a href="https://start.paypo.pl/" target="_blank">' + $t('Learn more') + '</a>');
             }
 
             if (gatewayId === model.gatewaysIds.visa_mobile) {
